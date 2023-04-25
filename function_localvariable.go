e6164d094c29dcbb793db017881587313040ec42
박두현 <doubled4017@gmail.com>
2023-04-25 13:20:25 +0900

    go first example

--- function_localvariable.go
+++ function_localvariable.go
@@ -12,5 +12,5 @@ func main() {
 		fmt.Println(m, s, g)
 	}
 
-	m = s + 20
+	//m = s + 20
 }
---
+++ if_initiate.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func getMyAge() (int, bool) {
+	return 19, true
+}
+
+func main() {
+	if age, ok := getMyAge(); ok && age < 20 {
+		fmt.Println("you are young", age)
+	} else if ok && age < 30 {
+		fmt.Println("Nice age", age)
+	} else if ok {
+		fmt.Println("You are beautiful", age)
+	} else {
+		fmt.Println("Error")
+	}
+
+	//fmt.Println("Your age is ", age)
+}
